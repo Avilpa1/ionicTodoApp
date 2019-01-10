efef7d508489dea66dbd31f2b8a649d429999d63
Paul Avila <pavil13@safeway.com>
2019-01-09 21:46:03 -0800

    Pages added

---
+++ TodoApp/src/pages/todo/todo.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the TodoPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-todo',
+  templateUrl: 'todo.html',
+})
+export class TodoPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TodoPage');
+  }
+
+}

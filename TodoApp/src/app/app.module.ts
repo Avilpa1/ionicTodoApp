43e5f8c45591e4bcc00c1bb9f1d4b7d3585fee59
Paul Avila <pavil13@safeway.com>
2019-01-10 04:59:31 -0800

    Added menu navigation

--- TodoApp/src/app/app.module.ts
+++ TodoApp/src/app/app.module.ts
@@ -1,33 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
 import { TodoPage } from '../pages/todo/todo';
+import { LoginPage } from '../pages/login/login';
 import { RegistrationPage } from '../pages/registration/registration';
+ 
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    LoginPage, 
     TodoPage,
+    LoginPage,
     RegistrationPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    LoginPage,
     TodoPage,
+    LoginPage,
     RegistrationPage
   ],
   providers: [
--- TodoApp/src/pages/login/login.ts
+++ TodoApp/src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
  * Generated class for the LoginPage page.
@@ -7,6 +7,7 @@ import { NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+
 
 @Component({
   selector: 'page-login',
